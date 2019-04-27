a67bd5fc66a0db1163143b8fd1d83631fd167cb8
Retika Gupta <retikagupta@gmail.com>
2019-04-28 00:30:22 +0100

    first commit

---
+++ TestAPI/Controllers/AuthorController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestAPI.Models;
+using Newtonsoft.Json;
+using System.Web.Http;
+
+namespace TestAPI.Controllers
+{
+
+    [ApiVersion("1.0")]
+    [Route("api/author")]
+    [ApiController]
+    public class AuthorV1Controller : ControllerBase
+    {
+        pubsContext pubs = new pubsContext();
+        [HttpGet]
+        [ProducesResponseType(200)]
+        public IActionResult GetAuthor(string name)
+        {
+            var author = pubs.Authors.Where(x => x.AuFname.Equals(name, StringComparison.OrdinalIgnoreCase)).ToList();
+            return Ok(author);
+            //throw new NotImplementedException();
+        }
+    }
+
+    [ApiVersion("2.0")]
+    [Route("api/author")]
+    [ApiController]
+    public class Authorv2Controller : ControllerBase
+    {
+        pubsContext pubs = new pubsContext();
+        [HttpGet]
+        [ProducesResponseType(202)]
+        public IActionResult GetAuthor(string name)
+        {
+            var author = pubs.Authors.Where(x => x.AuFname.Equals(name, StringComparison.OrdinalIgnoreCase)).ToList();
+            return Accepted(author);
+            //throw new NotImplementedException();
+        }
+    }
+}
---
+++ TestAPI/Startup.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using TestAPI.Filters;
+using TestAPI.Models;
+
+namespace TestAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc(options=> { options.Filters.Add<JsonExceptionFilter>(); options.Filters.Add<RequireHttpsOrCloseAttribute>(); }).SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            //to give the path to swagger document
+            services.AddSwaggerDocument();
+
+            //services.AddDbContext<pubsContext>(options => { options.UseInternalServiceProvider(_serviceprovider).});
+
+            //For API versioning
+            services.AddApiVersioning(options => {
+                options.ReportApiVersions = true;
+                options.AssumeDefaultVersionWhenUnspecified = true;
+                options.DefaultApiVersion = new ApiVersion(1, 0);
+                //This will select highest version
+                //options.ApiVersionSelector = new CurrentImplementationApiVersionSelector(options);
+                options.ApiVersionSelector = new LowestImplementedApiVersionSelector(options);
+            });
+            //to make url lowercase
+            services.AddRouting(options => options.LowercaseUrls = true);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUi3(options => {
+                    options.DocumentPath = "/swagger/v1/swagger.json";
+                });
+                app.UseApiVersioning();
+               
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            //app.UseHttpsRedirection();
+            app.UseMvc();
+            
+        }
+    }
+}
